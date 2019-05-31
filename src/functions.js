2afeb69fffcbc8ff4386fdd793369d3baf844065
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-05-31 11:23:43 -0700

    change age function to class with constructor

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,13 @@
-export function planetYears(birthday) {
+export class YearsOld {
+    constructor(dateBorn) {
+        this.dateBorn = dateBorn;
+    }
+    
+    earthYears() {
+        let birthday = new Date(this.dateBorn);
+        let today = new Date('2019-05-31');
+        let earthAge = new Date(Math.abs(today - birthday).getFullYear()
+    }
 
-}
+
+};
--- test/function.test.js
+++ test/function.test.js
@@ -1,10 +1,10 @@
-import { planetYears } from '../src/functions';
+import { YearsOld } from '../src/functions';
 
 describe('Users Age in Earth Years', function() {
 
   it('gets birthday from user and returns age in years', function() {
     // let birthday = new Date('1990-01-01');
     // let today = new Date('2019-05-31');
-    expect(planetYears(birthday)).toEqual(28);
+    expect(YearsOld.earthYears()).toEqual(28);
   });
 });
